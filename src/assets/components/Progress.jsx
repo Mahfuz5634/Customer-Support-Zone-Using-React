7ea80b097d3dbbeb05f2f9bd0211086fd988f81b
Mahfuz <mahfuzru5634@gmail.com>
2025-09-26 12:51:32 +0600

    added progress track section

---
+++ src/assets/components/Progress.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import photo1 from "../img/vector1.png";
+import photo2 from "../img/vector3.png";
+
+const Progress = () => {
+  return (
+    <div className="bg-[#f5f5f5]">
+      <div className="py-15 container gap-5 mx-auto flex justify-center item-center">
+        <div
+          className="bg-[#8539d1]  bg-no-repeat flex flex-col justify-center items-center rounded-2xl w-[650px] h-[220px]"
+          style={{
+            backgroundImage: `url(${photo1}), url(${photo2}),linear-gradient(to right, #7039e6, #995df1)`,
+
+            backgroundPosition: "left center, right center",
+            backgroundRepeat: "no-repeat, no-repeat",
+          }}
+        >
+          <h1 className="text-white text-3xl font-bold">In-Progress</h1>
+          <p className="text-2xl text-white font-semibold">0</p>
+        </div>
+        <div
+          className="bg-[#1f9e74] flex flex-col justify-center items-center rounded-2xl w-[650px] h-[220px]"
+          style={{
+            backgroundImage: `url(${photo1}), url(${photo2}),linear-gradient(to right, #54cf67, #30ae70)`,
+
+            backgroundPosition: "left center, right center",
+            backgroundRepeat: "no-repeat, no-repeat",
+          }}
+        >
+          <h1 className="text-white text-3xl font-bold">Resolved</h1>
+          <p className="text-2xl text-white font-semibold">0</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Progress;
